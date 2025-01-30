0941db5a3766aa5cf51b148c6fa3b32671299af7
LENOVO <abrenicamarvhil@gmail.com>
2025-01-30 21:15:41 +0800

    San Juan ACCFA

---
+++ Downloads/San Juan ACCFA Website/script.js
@@ -0,0 +1,10 @@
+// Function to toggle between the login and register forms
+function showRegisterForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+}
+
+function showLoginForm() {
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
